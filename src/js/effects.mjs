275413fd2a1a95f197e794a6da878a23c3ca48dc
Guillermo Morales <morales.guillermo2503@gmail.com>
2025-09-19 00:53:46 -0600

    Refactored css effects use

---
+++ src/js/effects.mjs
@@ -0,0 +1,12 @@
+/**
+ * CSS Effects that can be applied to components
+ */
+
+// Hover state
+export const hoverScale125 = "hover:scale-125";
+export const hoverIncreaseMargin= "hover:mx-2";
+export const hoverPrimary = "hover:text-primary";
+export const hoverTranslateRight = "hover:translate-x-3";
+
+// Transitions
+export const easeInFast = "transform  transition-transform duration-300 ease-in-out";
