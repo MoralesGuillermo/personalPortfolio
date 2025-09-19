1421454f4c4e8d865e8feff071f0b13bf0f481fa
Guillermo Morales <morales.guillermo2503@gmail.com>
2025-09-19 00:36:38 -0600

    Implemented skills sections

---
+++ src/data/Skills.mjs
@@ -0,0 +1,128 @@
+// Tech skills I manage
+// Data for Skills.astro
+
+/* Properties 
+    name: Icon's path (Check astro-icon for more details)
+    text: Skill text
+    color: Hex value to apply to the icon
+*/
+
+const skills = [
+    {
+        name: "tech/python",
+        text: "Python",
+        color: "",
+    },
+    {
+        name: "tech/fastapi",
+        text: "Fastapi",
+        color: "#009688",
+    },
+    {
+        name: "tech/java",
+        text: "Java",
+        color: "",
+    },
+    {
+        name: "tech/springboot",
+        text: "Springboot",
+        color: "#6DB33F",
+    },
+    {
+        name: "tech/php",
+        text: "PHP",
+        color: "#777BB4",
+    },
+    {
+        name: "tech/laravel",
+        text: "Laravel",
+        color: "#FF2D20",
+    },
+    {
+        name: "tech/nodedotjs",
+        text: "Node JS",
+        color: "#5FA04E",
+    },
+    {
+        name: "tech/html5",
+        text: "HTML",
+        color: "#E34F26",
+    },
+    {
+        name: "tech/css",
+        text: "CSS",
+        color: "#663399",
+    },
+    {
+        name: "tech/tailwindcss",
+        text: "Tailwind",
+        color: "#06B6D4",
+    },
+    {
+        name: "tech/bootstrap",
+        text: "Bootstrap",
+        color: "#7952B3",
+    },
+    {
+        name: "tech/astro",
+        text: "Astro JS",
+        color: "#BC52EE",
+    },
+    {
+        name: "tech/mysql",
+        text: "MySQL",
+        color: "#4479A1",
+    },
+    {
+        name: "tech/microsoft-sql-server-2",
+        text: "SQL Server",
+        color: "",
+    },
+    {
+        name: "tech/postgresql",
+        text: "PostgreSQL",
+        color: "#4169E1",
+    },
+    {
+        name: "tech/supabase",
+        text: "Supabase",
+        color: "#3FCF8E",
+    },
+    {
+        name: "tech/aws",
+        text: "AWS",
+        color: "#F68F1E",
+    },
+    {
+        name: "tech/ec2",
+        text: "AWS EC2",
+        color: "#FC390E",
+    },
+    {
+        name: "tech/git",
+        text: "Git",
+        color: "#F05032",
+    },
+    {
+        name: "tech/github",
+        text: "Github",
+        color: "#FFFFFF",
+    },
+    {
+        name: "tech/workflows",
+        text: "CI/CD",
+        color: "#F05032",
+    },
+        {
+        name: "tech/linux",
+        text: "Linux",
+        color: "#FCC624",
+    },
+        {
+        name: "tech/selenium",
+        text: "Selenium",
+        color: "#43B02A",
+    },
+];
+
+export default skills;
