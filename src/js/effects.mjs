0e160a2dade2a97cb568c30b810d28f619a799cf
Guillermo Morales <morales.guillermo2503@gmail.com>
2025-09-27 01:45:19 -0600

    Added Highlights section

--- src/js/effects.mjs
+++ src/js/effects.mjs
@@ -6,7 +6,9 @@
 export const hoverScale125 = "hover:scale-125";
 export const hoverIncreaseMargin= "hover:mx-2";
 export const hoverPrimary = "hover:text-primary";
+export const hoverPrimaryDark = "hover:text-primary-dark";
 export const hoverTranslateRight = "hover:translate-x-3";
 
 // Transitions
 export const easeInFast = "transform  transition-transform duration-300 ease-in-out";
+export const easeInSlow = "transition duration-600 ease-in-out";
