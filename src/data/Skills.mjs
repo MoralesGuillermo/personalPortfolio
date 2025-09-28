cc3607d072c18250527cdc39987d4d28642eb973
Guillermo Morales <morales.guillermo2503@gmail.com>
2025-09-28 14:35:53 -0600

    Implemented skills map for tech stack showcase

--- src/data/Skills.mjs
+++ src/data/Skills.mjs
@@ -6,123 +6,15 @@
     text: Skill text
     color: Hex value to apply to the icon
 */
+import backendSkills from "./backendSkills.mjs";
+import frontendSkills from "./frontendSkills.mjs";
+import databaseSkills from "./databaseSkills.mjs";
+import infrastructureSkills from "./infrastructureSkills.mjs";
+import languageSkills from "./languageSkills.mjs";
+import miscellaneousSkills from "./miscellaneousSkills.mjs";
+import versionControlSkills from "./versionControlSkills.mjs";
 
-const skills = [
-    {
-        name: "tech/python",
-        text: "Python",
-        color: "#3776AB",
-    },
-    {
-        name: "tech/fastapi",
-        text: "Fastapi",
-        color: "#009688",
-    },
-    {
-        name: "tech/java",
-        text: "Java",
-        color: "#f89820",
-    },
-    {
-        name: "tech/springboot",
-        text: "Springboot",
-        color: "#6DB33F",
-    },
-    {
-        name: "tech/php",
-        text: "PHP",
-        color: "#777BB4",
-    },
-    {
-        name: "tech/laravel",
-        text: "Laravel",
-        color: "#FF2D20",
-    },
-    {
-        name: "tech/nodedotjs",
-        text: "Node JS",
-        color: "#5FA04E",
-    },
-    {
-        name: "tech/html5",
-        text: "HTML",
-        color: "#E34F26",
-    },
-    {
-        name: "tech/css",
-        text: "CSS",
-        color: "#663399",
-    },
-    {
-        name: "tech/tailwindcss",
-        text: "Tailwind",
-        color: "#06B6D4",
-    },
-    {
-        name: "tech/bootstrap",
-        text: "Bootstrap",
-        color: "#7952B3",
-    },
-    {
-        name: "tech/astro",
-        text: "Astro JS",
-        color: "#BC52EE",
-    },
-    {
-        name: "tech/mysql",
-        text: "MySQL",
-        color: "#4479A1",
-    },
-    {
-        name: "tech/microsoft-sql-server-2",
-        text: "SQL Server",
-        color: "",
-    },
-    {
-        name: "tech/postgresql",
-        text: "PostgreSQL",
-        color: "#4169E1",
-    },
-    {
-        name: "tech/supabase",
-        text: "Supabase",
-        color: "#3FCF8E",
-    },
-    {
-        name: "tech/aws",
-        text: "AWS",
-        color: "#F68F1E",
-    },
-    {
-        name: "tech/ec2",
-        text: "AWS EC2",
-        color: "#FC390E",
-    },
-    {
-        name: "tech/git",
-        text: "Git",
-        color: "#F05032",
-    },
-    {
-        name: "tech/github",
-        text: "Github",
-        color: "#FFFFFF",
-    },
-    {
-        name: "tech/workflows",
-        text: "CI/CD",
-        color: "#F05032",
-    },
-        {
-        name: "tech/linux",
-        text: "Linux",
-        color: "#FCC624",
-    },
-        {
-        name: "tech/selenium",
-        text: "Selenium",
-        color: "#43B02A",
-    },
-];
+
+const skills = new Map([...backendSkills, ...frontendSkills, ...databaseSkills, ...infrastructureSkills, ...languageSkills, ...miscellaneousSkills, ...versionControlSkills])
 
 export default skills;
